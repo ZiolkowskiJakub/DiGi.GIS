9ea60dfdeab60cc85ae101399a07100400e52b1a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-16 09:21:44 +0100

    YearBuiltData implemented

---
+++ DiGi.GIS/Classes/YearBuiltData.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using DiGi.Core.Classes;
+using DiGi.GIS.Interfaces;
+
+namespace DiGi.GIS.Classes
+{
+    public class YearBuiltData : SerializableObject, IGISObject
+    {
+        [JsonInclude, JsonPropertyName("Reference")]
+        private string reference;
+
+        [JsonInclude, JsonPropertyName("Year")]
+        private short year;
+
+        public YearBuiltData(string reference, short year)
+        {
+            this.reference = reference;
+            this.year = year;
+        }
+
+        public YearBuiltData(YearBuiltData yearBuiltData)
+        {
+            if (yearBuiltData != null)
+            {
+                reference = yearBuiltData.reference;
+                year = yearBuiltData.year;
+            }
+        }
+
+        public YearBuiltData(JsonObject jsonObject)
+            : base(jsonObject)
+        {
+
+        }
+
+        [JsonIgnore]
+        public string Reference
+        {
+            get
+            {
+                return reference;
+            }
+        }
+
+        [JsonIgnore]
+        public short Year
+        {
+            get
+            {
+                return year;
+            }
+        }
+    }
+}
---
+++ DiGi.GIS/Classes/YearBuiltDataFile.cs
@@ -0,0 +1,26 @@
+﻿using DiGi.GIS.Interfaces;
+using System.Text.Json.Nodes;
+
+namespace DiGi.GIS.Classes
+{
+    public class YearBuiltDataFile : Core.IO.File.Classes.StorageFile<YearBuiltData>, IGISObject
+    {
+        public YearBuiltDataFile(YearBuiltDataFile yearBuiltDataFile)
+            : base(yearBuiltDataFile)
+        {
+
+        }
+
+        public YearBuiltDataFile(JsonObject jsonObject)
+            :base(jsonObject)
+        {
+
+        }
+
+        public YearBuiltDataFile(string path)
+            : base(path) 
+        {
+            
+        }
+    }
+}
