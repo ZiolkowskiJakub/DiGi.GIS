afdb826a369ec818c7ee935a5d38be9a8e4981b9
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-19 01:09:36 +0100

    FileTypeName constants added

---
+++ DiGi.GIS/Constans/FileTypeName.cs
@@ -0,0 +1,8 @@
+﻿namespace DiGi.GIS.Constans
+{
+    public static class FileTypeName
+    {
+        public const string GISModelFile = "GIS Model File";
+        public const string OrtoDatasFile = "Orto Datas File";
+    }
+}
