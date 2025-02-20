41b3ee1e538a6bf1d7172e2b19ba3f799293383e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-20 13:21:37 +0100

    Reference updated

--- DiGi.GIS/Modify/AddValue.cs
+++ DiGi.GIS/Modify/AddValue.cs
@@ -1,5 +1,6 @@
 ﻿using DiGi.Core.Classes;
 using DiGi.GIS.Classes;
+using DiGi.GIS.Interfaces;
 using System.Threading.Tasks;
 
 namespace DiGi.GIS
@@ -26,38 +27,6 @@ namespace DiGi.GIS
 
             UniqueReference result = ortoDatasFile.AddValue(ortoDatas);
             return result;
-        }
-
-        public static UniqueReference AddValue(this GISModelFile gISModelFile, Building2D builidng2D, int? yearBuilt)
-        {
-            if(gISModelFile == null || builidng2D == null)
-            {
-                return null;
-            }
-
-
-            string path = gISModelFile.Path;
-            if(string.IsNullOrWhiteSpace(path))
-            {
-                return null;
-            }
-
-            string directory = System.IO.Path.GetDirectoryName(path);
-            if(!System.IO.Directory.Exists(directory))
-            {
-                return null;
-            }
-
-            path = System.IO.Path.Combine(directory, string.Format("{0}.{1}", System.IO.Path.GetFileNameWithoutExtension(path), Constans.FileExtension.YearBuiltDatasFile));
-
-            using(YearBuiltDataFile yearBuiltDataFile = new YearBuiltDataFile(path))
-            {
-                yearBuiltDataFile.Open();
-
-                yearBuiltDataFile.De
-            }
-
-
         }
     }
 }
---
+++ DiGi.GIS/Modify/UpdateValue.cs
@@ -0,0 +1,54 @@
+﻿using DiGi.Core.Classes;
+using DiGi.GIS.Classes;
+using DiGi.GIS.Interfaces;
+
+namespace DiGi.GIS
+{
+    public static partial class Modify
+    {
+        public static UniqueReference UpdateValue(this GISModelFile gISModelFile, Building2D builidng2D, short? yearBuilt)
+        {
+            if(gISModelFile == null || builidng2D == null)
+            {
+                return null;
+            }
+            string reference = builidng2D.Reference;
+            if (string.IsNullOrWhiteSpace(reference))
+            {
+                return null;
+            }
+
+            string path = gISModelFile.Path;
+            if(string.IsNullOrWhiteSpace(path))
+            {
+                return null;
+            }
+
+            string directory = System.IO.Path.GetDirectoryName(path);
+            if(!System.IO.Directory.Exists(directory))
+            {
+                return null;
+            }
+
+            path = System.IO.Path.Combine(directory, string.Format("{0}.{1}", System.IO.Path.GetFileNameWithoutExtension(path), Constans.FileExtension.YearBuiltDatasFile));
+
+            using (YearBuiltDataFile yearBuiltDataFile = new YearBuiltDataFile(path))
+            {
+                yearBuiltDataFile.Open();
+
+                UniqueReference uniqueReference = yearBuiltDataFile.RemoveFirst<IYearBuiltData>(x => x.Reference == reference);
+                if (yearBuilt != null && yearBuilt.HasValue)
+                {
+                    uniqueReference = yearBuiltDataFile.AddValue(new YearBuiltData(builidng2D.Reference, yearBuilt.Value));
+                }
+
+                if (uniqueReference != null)
+                {
+                    yearBuiltDataFile.Save();
+                }
+
+                return uniqueReference;
+            }
+        }
+    }
+}
