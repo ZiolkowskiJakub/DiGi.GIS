72df0b4ee8580fba2aebd653901e9529f79c90b4
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-06-26 22:15:32 +0200

    EPWFile initial implementation

---
+++ DiGi.GIS/Query/Distance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DiGi.GIS
+{
+    public static partial class Query
+    {
+        public static double Distance(double latitude_1, double longitude_1, double latitude_2, double longitude_2)
+        {
+            if(double.IsNaN(latitude_1) || double.IsNaN(latitude_2) || double.IsNaN(longitude_1) || double.IsNaN(longitude_2))
+            {
+                return double.NaN;
+            }
+
+            double r = 6371000; // Earth radius in meters
+
+            double radLat1 = latitude_1 * Math.PI / 180.0; ;
+            double radLat2 = latitude_2 * Math.PI / 180.0; ;
+            double deltaLat = (latitude_2 - latitude_1) * Math.PI / 180.0;
+            double deltaLon = (longitude_2 - longitude_1) * Math.PI / 180.0;
+
+            double a = Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2) +
+                       Math.Cos(radLat1) * Math.Cos(radLat2) *
+                       Math.Sin(deltaLon / 2) * Math.Sin(deltaLon / 2);
+            
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return r * c;
+        }
+    }
+}
+
---
+++ DiGi.GIS/Query/EPWFile.cs
@@ -0,0 +1,52 @@
+﻿using DiGi.EPW.Classes;
+using DiGi.Geometry.Planar.Classes;
+using DiGi.Geometry.Spatial.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace DiGi.GIS
+{
+    public static partial class Query
+    {
+        public static EPWFile EPWFile(this Point2D point2D, IEnumerable<EPWFile> ePWFiles, out double distance)
+        {
+            distance = double.NaN;
+
+            if(point2D == null || ePWFiles == null)
+            {
+                return null;
+            }
+
+            Point3D point3D = Convert.ToEPSG4326(point2D);
+
+            List<Tuple<double, Point3D, EPWFile>> tuples = new List<Tuple<double, Point3D, EPWFile>>();
+
+            foreach (EPWFile ePWFile in ePWFiles)
+            {
+                Location location = ePWFile?.Location;
+                if(location == null)
+                {
+                    continue;
+                }
+
+                Point3D point3D_EPWFile = new Point3D(location.Longitude, location.Latitude, location.Elevation);
+
+                tuples.Add(new Tuple<double, Point3D, EPWFile>(point3D.Distance(point3D_EPWFile), point3D_EPWFile, ePWFile));
+            }
+
+            if(tuples == null || tuples.Count == 0)
+            {
+                return null;
+            }
+
+            tuples.Sort((x, y) => x.Item1.CompareTo(y.Item1));
+
+
+            Tuple<double, Point3D, EPWFile> tuple = tuples[0];
+
+            distance = Distance(point3D.Y, point3D.X, tuple.Item2.Y, tuple.Item2.X);
+
+            return tuple.Item3;
+        }
+    }
+}
