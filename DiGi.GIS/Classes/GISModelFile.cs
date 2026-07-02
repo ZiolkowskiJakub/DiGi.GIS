1caf2242290cf9080c40876855f4c684be53179d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-07-02 18:55:53 +0200

    Files renamed

--- DiGi.GIS/Classes/GISModelFile.cs
+++ DiGi.GIS/Classes/GISModelFile.cs
@@ -1,38 +1,48 @@
-﻿using DiGi.GIS.Interfaces;
+using DiGi.Core.Classes;
+using DiGi.GIS.Interfaces;
 using System.Text.Json.Nodes;
 
 namespace DiGi.GIS.Classes
 {
     /// <summary>
-    /// Represents a file containing statistical unit data.
+    /// Represents a GIS model file that stores GIS model data with external reference support
     /// </summary>
-    public class StatisticalUnitFile : Core.IO.File.Classes.ValueFile<StatisticalUnit>, IGISObject
+    public class GISModelFile : Core.IO.File.Classes.ValueFile<GISModel>, IGISObject
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="StatisticalUnitFile"/> class based on an existing instance.
+        /// Initializes a new instance of the GISModelFile class by copying another instance
         /// </summary>
-        /// <param name="statisticalUnitFile">The source StatisticalUnitFile instance to copy from.</param>
-        public StatisticalUnitFile(StatisticalUnitFile? statisticalUnitFile)
-            : base(statisticalUnitFile)
+        /// <param name="gISModelFile">The source GIS model file to copy from.</param>
+        public GISModelFile(GISModelFile? gISModelFile)
+            : base(gISModelFile)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="StatisticalUnitFile"/> class using a JSON object.
+        /// Initializes a new instance of the GISModelFile class from a JSON object
         /// </summary>
-        /// <param name="jsonObject">The JSON object containing the file data.</param>
-        public StatisticalUnitFile(JsonObject? jsonObject)
+        /// <param name="jsonObject">The JSON object containing the data for the GIS model file.</param>
+        public GISModelFile(JsonObject? jsonObject)
             : base(jsonObject)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="StatisticalUnitFile"/> class from the specified path.
+        /// Initializes a new instance of the GISModelFile class from the specified path
         /// </summary>
-        /// <param name="path">The file system path to the statistical unit file.</param>
-        public StatisticalUnitFile(string? path)
+        /// <param name="path">The file system path to the GIS model file.</param>
+        public GISModelFile(string? path)
             : base(path)
         {
+        }
+
+        /// <summary> Gets the external reference for this GIS model file </summary>
+        public GuidExternalReference? GuidExternalReference
+        {
+            get
+            {
+                return new GuidExternalReference(Path, Value);
+            }
         }
     }
 }
--- DiGi.GIS/Classes/StatisticalUnitFile.cs
+++ DiGi.GIS/Classes/StatisticalUnitFile.cs
@@ -1,48 +1,38 @@
-using DiGi.Core.Classes;
 using DiGi.GIS.Interfaces;
 using System.Text.Json.Nodes;
 
 namespace DiGi.GIS.Classes
 {
     /// <summary>
-    /// Represents a GIS model file that stores GIS model data with external reference support
+    /// Represents a file containing statistical unit data.
     /// </summary>
-    public class GISModelFile : Core.IO.File.Classes.ValueFile<GISModel>, IGISObject
+    public class StatisticalUnitFile : Core.IO.File.Classes.ValueFile<StatisticalUnit>, IGISObject
     {
         /// <summary>
-        /// Initializes a new instance of the GISModelFile class by copying another instance
+        /// Initializes a new instance of the <see cref="StatisticalUnitFile"/> class based on an existing instance.
         /// </summary>
-        /// <param name="gISModelFile">The source GIS model file to copy from.</param>
-        public GISModelFile(GISModelFile? gISModelFile)
-            : base(gISModelFile)
+        /// <param name="statisticalUnitFile">The source StatisticalUnitFile instance to copy from.</param>
+        public StatisticalUnitFile(StatisticalUnitFile? statisticalUnitFile)
+            : base(statisticalUnitFile)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the GISModelFile class from a JSON object
+        /// Initializes a new instance of the <see cref="StatisticalUnitFile"/> class using a JSON object.
         /// </summary>
-        /// <param name="jsonObject">The JSON object containing the data for the GIS model file.</param>
-        public GISModelFile(JsonObject? jsonObject)
+        /// <param name="jsonObject">The JSON object containing the file data.</param>
+        public StatisticalUnitFile(JsonObject? jsonObject)
             : base(jsonObject)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the GISModelFile class from the specified path
+        /// Initializes a new instance of the <see cref="StatisticalUnitFile"/> class from the specified path.
         /// </summary>
-        /// <param name="path">The file system path to the GIS model file.</param>
-        public GISModelFile(string? path)
+        /// <param name="path">The file system path to the statistical unit file.</param>
+        public StatisticalUnitFile(string? path)
             : base(path)
         {
-        }
-
-        /// <summary> Gets the external reference for this GIS model file </summary>
-        public GuidExternalReference? GuidExternalReference
-        {
-            get
-            {
-                return new GuidExternalReference(Path, Value);
-            }
         }
     }
 }
